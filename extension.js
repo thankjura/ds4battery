453c50ea55c098bf70bcf186c9b56bfdd83b83c1
thankjura <me@slie.ru>
2017-08-20 19:02:34 +0300

    second

--- extension.js
+++ extension.js
@@ -3,31 +3,29 @@ const Lang = imports.lang;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const Main = imports.ui.main;
+const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Util = imports.misc.util;
 const Mainloop = imports.mainloop;
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Clutter = imports.gi.Clutter;
 
-function DS4Battery() {
-    this._init.apply(this, arguments);
-}
+const DEFAULT_ICON = "ds4-symbolic";
+const CHARGING_ICON = "ds4-charging-symbolic";
 
-DS4Battery.prototype = {
-    __proto__: PanelMenu.Button.prototype,
+const DS4Battery = new Lang.Class({
+    Name: "DualShock 4 Power",
+    Extends: PanelMenu.Button,
 
-    _init: function(){
+    _init: function() {
         PanelMenu.Button.prototype._init.call(this, 0, 'ds4battery');
 
-        // Set Logo
-        var theme = imports.gi.Gtk.IconTheme.get_default();
-        let icon_dir = Extension.dir.get_child('icons');
-        theme.append_search_path(icon_dir.get_path());
-        this._logo = new St.Icon({ icon_name: 'ds4', style_class: 'system-status-icon'});
+        this._icon = new St.Icon({
+            icon_name: DEFAULT_ICON,
+            style_class: 'system-status-icon',
 
-        this.lang = {
-            'ds4' : 'Dual Shock 4 power'
-        };
+        });
+
         this.statusLabel = new St.Label({
             y_align: Clutter.ActorAlign.CENTER,
             text: "--",
@@ -36,22 +34,19 @@ DS4Battery.prototype = {
 
         this._destroy();
 
-        let box = new St.BoxLayout({ name: 'ds4Box' });
-        this.actor.add_actor(box);
-        box.add_actor(this._logo);
-        box.add_actor(this.statusLabel);
+        let box = new St.BoxLayout({ name: 'ds4Box' , vertical: false });
+        box.add_actor(this._icon);
+		box.add_actor(this.statusLabel);
+		this.actor.add_actor(box);
 
-        this.upowerPath = this._detectUpower();
-        this.devicePath = this._detectDevice();
-        this.command=["xdg-open", "http://slie.ru/"];
         if(!this.sensorsPath) {
             this.title='Warning';
             this.content='Please install UPower';
         } 
 
-        this._updatePower();
-        event = GLib.timeout_add_seconds(0, 15, Lang.bind(this, function () {
-            this._updatePower();
+        this._updateStatus();
+        event = GLib.timeout_add_seconds(0, 5, Lang.bind(this, function () {
+            this._updateStatus();
             return true;
         }));
     },
@@ -59,68 +54,59 @@ DS4Battery.prototype = {
     _destroy: function() {
         this.menu.box.get_children().forEach(function(c) {
             c.destroy();
+        });
+    },
+
+    _updateStatus: function() {        
+        let powerDir = Gio.File.new_for_path("/sys/class/power_supply");
+
+        let fileEnum;
+        try {
+            fileEnum = powerDir.enumerate_children('standard::name,standard::type', Gio.FileQueryInfoFlags.NONE, null);
+        } catch (e) {
+            fileEnum = null;
         }
-    }
 
-    _detectUpower: function() {
-        // detect upower
-        let ret = GLib.find_program_in_path("upower");
-        return ret; // path to upower
-    },
-
-    _detectDevice: function() {        
-        if (this.upowerPath) {
-            let upowerOutput = GLib.spawn_command_line_sync(this.upowerPath + " -e|grep sony_controller_battery");
-            if (upowerOutput) {
-                return upowerOutput[0];
-            }
-        } 
-        return false;
-    },
-
-    _updatePower: function() {
-        let items = new Array();
-        let powerInfo = null;
-        if (this.upowerPath) {
-            if (!this.devicePath) {
-                this._detectDevice();
-            }
-
-            if (this.devicePath) {
-                let upowerOutput = GLib.spawn_command_line_sync(this.sensorsPath + " -i " + this.devicePath + " | grep percentage");
-                if (upowerOutput) {
-                    this._destroy();
-                    this.title = upowerOutput.replace(/[\D]+/, "");
+        if (fileEnum != null) {
+            let info;
+            while ((info = fileEnum.next_file(null))) {
+                if (info.get_name().startsWith("sony_controller_battery_")) {
+                    let powerPath = GLib.build_filenamev([powerDir.get_path(), info.get_name(), "capacity"]);
+                    let statusPath = GLib.build_filenamev([powerDir.get_path(), info.get_name(), "status"]);
+                    let powerFile = Gio.File.new_for_path(powerPath);
+                    let statusFile = Gio.File.new_for_path(statusPath);
+                    let powerValue = GLib.spawn_command_line_sync("cat " + powerFile.get_path());
+                    let statusValue = GLib.spawn_command_line_sync("cat " + statusFile.get_path());
+                    let power = powerValue[1].toString().replace("\n", "");
+                    
+                    this.statusLabel.set_text(power + "%");
+                    
+                    if (statusValue[1].toString().indexOf("Charging") > -1) {
+                        this._icon.icon_name = CHARGING_ICON;
+                    } else {
+                        this._icon.icon_name = DEFAULT_ICON;
+                    }
+                    
+                    this.actor.show();
+                    return;
                 }
             }
         }
-        
-        this._destroy();
-        
-        let section = new PopupMenu.PopupMenuSection("");
-        let item = new PopupMenu.PopupMenuItem("", { reactive: false });
-        item.actor.add_child(new St.Label({
-                    text: "test",
-                    style_class: "sm-label"
-        }));
-        section.addMenuItem(item);
-        this.menu.addMenuItem(section);
-    },
 
-    _getContent: function(c){
-        return c.toString();
+        
+        this.actor.hide();
     },
-
-}
+});
 
 // for debugging
-function debug(a){
+function debug(a) {
+    a = "ds4: " + a;
     global.log(a);
-    Util.spawn(['echo',a]);
 }
 
-function init() {
-// do nothing
+function init(extensionMeta) {
+    let theme = imports.gi.Gtk.IconTheme.get_default();
+    theme.append_search_path(extensionMeta.path + "/icons");
 }
 
 let indicator;
